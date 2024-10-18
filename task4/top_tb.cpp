00d61254762dd429518a94da343d577e931e887a
maxryan4 <maxryan428@gmail.com>
2024-10-18 17:34:19 +0100

    Task 4

---
+++ task4/top_tb.cpp
@@ -0,0 +1,57 @@
+#include "Vtop.h" // changed to vtop
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env)
+{
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vtop *top = new Vtop; // changed to vtop
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("top.vcd"); // changed to top.vcd
+
+    // init Vbuddy
+    if (vbdOpen() != 1)
+        return (-1);
+    vbdHeader("Lab 1: Counter");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 0;
+    vbdSetMode(1);
+
+    // run simulations for many clock cycles
+    for (i = 0; i < 300; i++)
+    {
+
+        // dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++)
+        {
+            tfp->dump(2 * i + clk); // unit is in ps!!!
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        // ++++ Send bcd value to Vbuddy
+        vbdHex(4, (int(top->bcd) >> 16) & 0xF);
+        vbdHex(3, (int(top->bcd) >> 8) & 0xF);
+        vbdHex(2, (int(top->bcd) >> 4) & 0xF);
+        vbdHex(1, int(top->bcd) & 0xF);
+        // ---- end of Vbuddy output section
+
+        top->en = vbdFlag();
+
+        if (Verilated::gotFinish())
+            exit(0);
+    }
+    vbdClose(); // ++++
+    tfp->close();
+    exit(0);
+}
